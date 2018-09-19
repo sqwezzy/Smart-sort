c461e8faaaaa9c107a35855d106575a60905ab14
sqwezzy <maks.subbotko46@gmail.com>
2018-09-19 14:18:13 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,30 +1,44 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.elements = [];
+    this.compareFunction = (a, b) => a - b;
   }
 
   add(element) {
-    // your implementation
+    this.elements.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.elements[index];
   }
 
   get length() {
-    // your implementation
+    return this.elements.length;
   }
 
   toArray() {
-    // your implementation
+    return this.elements;
   }
 
   sort(indices) {
-    // your implementation
+    this.helpArr = [];
+    indices = indices.sort((a, b) => a - b);
+
+    indices.forEach((element, index) => {
+      this.helpArr[index] = this.elements[element];
+    });
+
+    this.helpArr.sort(this.compareFunction);
+    this.helpArr.sort(this.compareFunction);
+
+    indices.forEach((element, index) => {
+      this.elements[element] = this.helpArr[index];
+    })
+
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.compareFunction = compareFunction;
   }
 }
 
